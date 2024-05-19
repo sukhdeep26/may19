dc36e26342a1230354492022cca54b24461360db
Cloud User <ec2-user@ip-172-31-34-174.us-east-2.compute.internal>
2024-05-19 19:13:44 +0000

    Adding prohect files

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    rv = client.get('/')
+    assert rv.data == b'Hello, DevSecOps!'
+    assert rv.status_code == 200
+
